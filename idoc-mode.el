9d87e504dfe5087d2b4c7983e8ac4d43b5b60674
Endy <livm04@hotmail.com>
2026-09-12 03:33:35 +0100

    Added an emacs idoc-mode for editing idoc files

---
+++ idoc-mode.el
@@ -0,0 +1,22 @@
+;;; idoc-mode --- Major mode for idoc files
+
+;;;###autoload
+(define-derived-mode idoc-mode fundamental-mode "Idoc"
+  "Major mode for idoc files"
+  (setq-local font-lock-defaults '((idoc-font-lock-keywords))))
+
+
+(defconst idoc-font-lock-keywords
+  '(
+    ;; Sections (keywords)
+    ("\\_<\\([[:word:]_-]+\\):" 1 font-lock-keyword-face)
+    ;; Booleans
+    ("=[[:space:]]*\\(true\\|false\\)\\_>" 1 font-lock-keyword-face)
+    ;; Comments
+    ("#.*$" . font-lock-comment-face)
+    ))
+
+
+(add-to-list 'auto-mode-alist '("\\.idoc\\'" . 'idoc-mode))
+
+(provide 'idoc-mode)
